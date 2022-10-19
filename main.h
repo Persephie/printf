17c8d6111a4d34173716d9883477bc94fad3ef04
bravin-atonya <bravinatonya.brani@gmail.com>
2022-10-19 09:26:51 -0700

    test for NULL function

--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 int _printf(const char *format, ...);
 int print_string(char *parameter_content);
 int _putchar(char c);
+void NULL_test(char *parameter_content, int *char_param_counter);
 
 #endif
 
