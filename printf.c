a2122c8c22f271b62bb8dd22240446fe43030bff
bravin-atonya <bravinatonya.brani@gmail.com>
2022-10-19 14:28:52 -0700

    correct out put of null string

--- printf.c
+++ printf.c
@@ -67,7 +67,7 @@ int _printf(const char *format, ...)
 	va_list parameter_list;
 
 	va_start(parameter_list, format);
-	if (format != NULL)
+	if (format == NULL)
 		return (-1);
 	for (char_counter = 0; format && format[char_counter] != '\0'; char_counter++)
 	{
