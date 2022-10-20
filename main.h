0946efed92cdfe2ec45b207bf9453e8d4f9e8b42
Maureen <isagimaureen@gmail.com>
2022-10-20 10:20:43 -0700

    Task

--- main.h
+++ main.h
@@ -67,14 +67,6 @@ typedef struct specifier
 	int (*f)(va_list, params_t *);
 } specifier_t;
 
-/* specifiers */
-int _printf(const char *format, ...);
-int print_string(char *parameter_content);
-int _putchar(char c);
-void NULL_test(char *parameter_content, int *char_param_counter);
-int end_with_error(int error_number);
-void test_null_char(char null_character, int *char_param_counter);
-
 /* _put.c module */
 int _puts(char *str);
 int _putchar(int c);
