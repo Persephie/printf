f192b6af3e12701be395155a500cba1fca2dfed0
bravin-atonya <bravinatonya.brani@gmail.com>
2022-10-20 01:37:16 -0700

    correction on null string output

--- printf.c
+++ printf.c
@@ -75,7 +75,7 @@ int _printf(const char *format, ...)
 
 	va_start(parameter_list, format);
 	if (format == NULL)
-		cahr_param_ctr += print_string("(null)");
+		return (-1);
 	for (char_counter = 0; format && format[char_counter] != '\0'; char_counter++)
 	{
 		if (format[char_counter] != '%')
